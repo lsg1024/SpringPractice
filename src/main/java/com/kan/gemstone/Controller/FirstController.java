1b874d9e1834d36781476723820114a7053996e9
JS <zks145@naver.com>
2023-07-19 23:04:04 +0900

    java-springboot 연습

---
+++ src/main/java/com/kan/gemstone/Controller/FirstController.java
@@ -0,0 +1,22 @@
+package com.kan.gemstone.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class FirstController {
+
+    @GetMapping("/hi")
+    public String niceToMeetYou(Model model) {
+        model.addAttribute("username", "임재성");
+        return "greetings"; // templates/greetins.mustach -> 브라우저 전송
+    }
+
+    @GetMapping("/bye")
+    public String byeToYou(Model model) {
+        model.addAttribute("byename", "재성임");
+        return "goodbye";
+    }
+
+}
---
+++ src/main/java/com/kan/gemstone/GemstoneApplication.java
@@ -0,0 +1,13 @@
+package com.kan.gemstone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GemstoneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GemstoneApplication.class, args);
+	}
+
+}
